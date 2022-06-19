ab65b55c5627ea4e81df7a84626ace6c4ae5563a
macpat83 <macpat@gmail.com>
2022-06-19 10:34:21 -0500

    created post routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,6 @@
-
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // GET /api/users
 router.get('/', (req, res) => {
