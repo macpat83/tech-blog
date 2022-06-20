127021739f594a1a842523dbf9f5996634a81720
macpat83 <macpat@gmail.com>
2022-06-20 18:39:52 -0500

    added sessions for authentication

--- server.js
+++ server.js
@@ -6,9 +6,27 @@ const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
+const session = require('express-session');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'WhoDatSaidDeyGonBeatThemSaints',
+  cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
 
 
 app.use(express.json());
